68a9a4b426f71c982a383725e534a652d21961db
unknown <BatLo264@ltpemdomain.local>
2019-10-03 10:15:22 +0200

    95rfvg

---
+++ main.js
@@ -0,0 +1,29 @@
+/*var person = prompt("Please enter your name");
+var age = prompt("please enter your age");
+alert("Hello " + person + "Next year you will be " + (Number(age) + 1));*/
+
+/*var FirstNumber = prompt("Please enter your first number ");
+var SecondNumber = prompt("Please enter your second number");
+
+if (FirstNumber == SecondNumber) {
+    alert("Your Numbers are equal");
+}
+if (FirstNumber !== SecondNumber) {
+    alert("Your Numbers are not equal");
+}*/
+var z = prompt("Do you have a Diploma?");
+var x = prompt("How old are you?");
+if (x < 20) {
+    alert("Good for you ");
+}
+
+if (x < 20) {
+    alert("GO study");
+}
+
+if (z.toLocaleLowerCase() == "yes") {
+    alert("You are ready to work");
+}
+if (z.toLocaleLowerCase() == "no") {
+    alert("You are not ready");
+}

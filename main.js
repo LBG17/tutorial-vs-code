28cb1dfe702b25d4a89d01110ab302436ac8e57d
LBG17 <lbglux@gmail.com>
2019-10-18 11:06:07 +0200

    1

--- main.js
+++ main.js
@@ -33,11 +33,11 @@ if (z.toLocaleLowerCase() == "no") {
     console.log(i);
 }*/
 
-function SolveEx2() {
+/*function SolveEx2() {
     var d = document.getElementById("SumNumbers100");
     var sum = 0;
     for (i = 0; i <= 10; i = i + 1) {
         sum = sum + i;
     }
     d.innerHTML = d.innerHTML + "<br>" + sum.toString();
-}
+}*/
